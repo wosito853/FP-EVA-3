f31ef2d85ec949b824240d19a29e45979706ef23
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-11-04 17:52:14 -0600

    Add files via upload

---
+++ EVA3_1_METODO/src/main/java/com/mycompany/eva3_1_metodo/EVA3_1_METODO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA3_1_METODO {
+    public static void main(String[] args) {
+        // uso de los metodos: 
+        // INVOCACION O LLAMADA
+       imprimirMensaje("hola mundo!");
+    }
+    // DECLARACION
+    // header (encabezado)
+    // IMPRIMIR UN MENSAJE
+    // JAVA: inicir en minuscu, verbo, siguientes palabras en mayusc
+    public static void imprimirMensaje(String mensaje){
+        System.out.println("TU MENSAJE:" + mensaje);
+        
+
+    }
+}
